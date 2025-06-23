12e1531b2b86aa5b9458689b5577f1fe0949cfaa
Jason Li <jasonli3@lsu.edu>
2025-06-23 18:51:33 -0500

    Cleared streaming logic. Added disabling certain components while streaming to avoid errors.

--- main.py
+++ main.py
@@ -157,7 +157,7 @@ class OllamaOnDemandUI:
     # Event handler
     #------------------------------------------------------------------
     
-    def _stream_chat(self):
+    def stream_chat(self):
         """
         Stream chat (invoked by new/edit/retry).
         
@@ -167,26 +167,71 @@ class OllamaOnDemandUI:
             chat_history:       Current chat history
             user_input:         Update user input field to "" and button face
         """
+        
+        # Failsafe: Only stream while is_streaming is True
+        if self.is_streaming:
 
-        # Generate next chat results
-        response = self.client.chat(
-            model = self.model_selected,
-            messages = self.chat_history,
-            stream = True
-        )
+            # Generate next chat results
+            response = self.client.chat(
+                model = self.model_selected,
+                messages = self.chat_history,
+                stream = True
+            )
 
-        # Stream results in chunks while not interrupted
-        for chunk in response:
-            if not self.is_streaming:
-                break
-            delta = chunk.get("message", {}).get("content", "")
-            delta = delta.replace("<think>", "(Thinking...)").replace("</think>", "(/Thinking...)")
-            self.chat_history[-1]["content"] += delta
-            yield self.chat_history, gr.update(value="", submit_btn=False, stop_btn=True)
+            # Stream results in chunks while not interrupted
+            for chunk in response:
+                if not self.is_streaming:
+                    break
+                delta = chunk.get("message", {}).get("content", "")
+                delta = delta.replace("<think>", "(Thinking...)").replace("</think>", "(/Thinking...)")
+                self.chat_history[-1]["content"] += delta
+                yield self.chat_history, gr.update(value="", submit_btn=False, stop_btn=True)
+        
+        # Once finished, set streaming to False
+        self.is_streaming = False
+        
+        # Final update components
+        yield self.chat_history, gr.update(value="", submit_btn=True, stop_btn=False)
+    
+    def stop_stream_chat(self):
+        """
+        Stop streaming.
+        
+        Input:
+            None
+        Output: 
+            chat_history:       Current chat history
+            user_input:         Update user input field to "" and button face
+        """
+        
+        # Set streaming to False
+        self.is_streaming = False
+        
+        # Update components
+        yield self.chat_history, gr.update(value="", submit_btn=True, stop_btn=False)
+    
+    def disable_enable_components(self, N):
+        """
+        Disable or enable components depending on whether it is streaming.
+        
+        Input:
+            N:                  Number of updates
+        Output: 
+            component * N:      N updates
+        """
+        
+        # Set updates base on streaming status
+        if self.is_streaming:
+            update = gr.update(interactive=False)
+        else:
+            update = gr.update(interactive=True)
+        
+        # Update components
+        return tuple([update] * N)
     
     def new_message(self, user_message):
         """
-        New user message
+        When a new message is submitted, set chatbot & input field before start streaming
         
         Input:
             user_message:       User's input
@@ -194,27 +239,20 @@ class OllamaOnDemandUI:
             chat_history:       Current chat history
             user_input:         Update user input field to "" and button face
         """
-
-        # If currently not streaming, start streaming
-        if not self.is_streaming:
-            
-            # Set streaming to True
-            self.is_streaming = True
-            
-            # Append user message to chat history
-            self.chat_history.append({"role": "user", "content": user_message["text"]})
-            self.chat_history.append({"role": "assistant", "content": ""})
-            yield self.chat_history, gr.update(value="", submit_btn=False, stop_btn=True)
-
-            # Generate next chat results
-            yield from self._stream_chat()
         
-        self.is_streaming = False
-        yield self.chat_history, gr.update(value="", submit_btn=True, stop_btn=False)
+        # Append user message to chat history
+        self.chat_history.append({"role": "user", "content": user_message["text"]})
+        self.chat_history.append({"role": "assistant", "content": ""})
+            
+        # Set streaming to True
+        self.is_streaming = True
+        
+        # Update components
+        yield self.chat_history, gr.update(value="", submit_btn=False, stop_btn=True)
     
     def retry(self, retry_data: gr.RetryData):
         """
-        New user message
+        When retry request is sent, set chatbot & input field before start streaming
         
         Input:
             retry_data:         Event instance (as gr.RetryData)
@@ -222,23 +260,16 @@ class OllamaOnDemandUI:
             chat_history:       Current chat history
             user_input:         Update user input field to "" and button face
         """
-
-        # If currently not streaming, start streaming
-        if not self.is_streaming:
-            
-            # Set to streaming and continue
-            self.is_streaming = True
-            
-            # Append user message to chat history
-            self.chat_history = self.chat_history[:retry_data.index+1]
-            self.chat_history.append({"role": "assistant", "content": ""})
-            yield self.chat_history, gr.update(value="", submit_btn=False, stop_btn=True)
-
-            # Generate next chat results
-            yield from self._stream_chat()
         
-        self.is_streaming = False
-        yield self.chat_history, gr.update(value="", submit_btn=True, stop_btn=False)
+        # Append user message to chat history
+        self.chat_history[:] = self.chat_history[:retry_data.index+1]
+        self.chat_history.append({"role": "assistant", "content": ""})
+            
+        # Set to streaming and continue
+        self.is_streaming = True
+        
+        # Update components
+        yield self.chat_history, gr.update(value="", submit_btn=False, stop_btn=True)
         
     def update_chat_selector(self):
         """
@@ -339,7 +370,6 @@ class OllamaOnDemandUI:
             self.update_current_chat(self.chat_index)
         
         return gr.update(choices=cs.get_chat_titles(), value=cs.get_chat_titles()[self.chat_index]), self.chat_history
-
     
     
     #------------------------------------------------------------------
@@ -402,7 +432,7 @@ class OllamaOnDemandUI:
                 
                 #Input field (multimodal)
                 user_input = gr.MultimodalTextbox(
-                    placeholder="Type your message here…", 
+                    placeholder="Ask anything", 
                     submit_btn=True,
                     stop_btn=False,
                     show_label=False
@@ -488,35 +518,68 @@ class OllamaOnDemandUI:
             
             # Chatbot: Retry
             chatbot.retry(
-                fn=self.retry,                      # Retry handler
+                fn=self.retry,                      # Prepare to retry
                 inputs=[],
                 outputs=[chatbot, user_input]
             ).then(
-                fn=self.update_chat_selector,       # Then update chat title if needed
+                fn=lambda : self.disable_enable_components(3),  
+                                                    # Disable certain components
+                inputs=[],
+                outputs=[chat_selector, new_btn, del_btn]
+            ).then(
+                fn=self.stream_chat,                # Start streaming
+                inputs=[],
+                outputs=[chatbot, user_input]
+            ).then(
+                fn=self.update_chat_selector,       # Update chat title if needed
                 inputs=[],
                 outputs=[chat_selector]
+            ).then(
+                fn=lambda : self.disable_enable_components(3),  
+                                                    # Re-enable disabled components
+                inputs=[],
+                outputs=[chat_selector, new_btn, del_btn]
             )
 
             # User input: Submit new message
             user_input.submit(
-                fn=self.new_message,                # New message handler
+                fn=self.new_message,                # Prepare to process new message
                 inputs=[user_input],
                 outputs=[chatbot, user_input]
             ).then(
-                fn=self.update_chat_selector,       # Then update chat title if needed
+                fn=lambda : self.disable_enable_components(3),  
+                                                    # Disable certain components
+                inputs=[],
+                outputs=[chat_selector, new_btn, del_btn]
+            ).then(
+                fn=self.stream_chat,                # Start streaming
+                inputs=[],
+                outputs=[chatbot, user_input]
+            ).then(
+                fn=self.update_chat_selector,       # Update chat title if needed
                 inputs=[],
                 outputs=[chat_selector]
+            ).then(
+                fn=lambda : self.disable_enable_components(3),  
+                                                    # Re-enable disabled components
+                inputs=[],
+                outputs=[chat_selector, new_btn, del_btn]
             )
             
             # User input: Stop
             user_input.stop(
-                fn=self.new_message,                # Stop streaming (run new_message again while self.is_streaming=True)
-                inputs=[user_input],
+                fn=self.stop_stream_chat,           # Stop streaming (run new_message again while self.is_streaming=True)
+                inputs=[],
                 outputs=[chatbot, user_input]
             ).then(
-                fn=self.update_chat_selector,       # Then update chat title if needed
+                fn=self.update_chat_selector,       # Update chat title if needed
                 inputs=[],
-                outputs=[chat_selector]
+                outputs=[chat_selector, new_btn, del_btn]
+            ).then(
+                fn=lambda : self.disable_enable_components(3),  
+                                                    # Re-enable disabled components
+                inputs=[],
+                outputs=[chat_selector, new_btn, del_btn]
             )
 
             #----------------------------------------------------------
